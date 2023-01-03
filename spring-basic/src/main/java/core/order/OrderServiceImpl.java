675ed8f4ece2f3c046abbe41803907d95e241c6d
JeonSangYong <tkddyd420@naver.com>
2023-01-03 20:04:31 +0900

    [강의] 주문과 할인 도메인 개발

---
+++ spring-basic/src/main/java/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package core.order;
+
+import core.discount.DiscountPolicy;
+import core.discount.FixDiscountPolicy;
+import core.member.Member;
+import core.member.MemberRepository;
+import core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
