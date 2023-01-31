7cf50ba809144b6e499624c1d4a3b0fde8554a8c
JeonSangYong <tkddyd420@naver.com>
2023-01-31 17:17:33 +0900

    [강의] 다양한 의존관계 주입 방법

--- spring-basic/src/main/java/core/AutoAppConfig.java
+++ spring-basic/src/main/java/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package core;
 
+import core.member.MemberRepository;
+import core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
