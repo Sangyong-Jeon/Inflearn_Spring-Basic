d4628068636e40d6c497c11a84631e211052a819
JeonSangYong <tkddyd420@naver.com>
2023-01-31 20:48:58 +0900

    [강의] 생성자 주입을 선택해라!

---
+++ spring-basic/src/test/java/core/order/OrderServiceImplTest.java
@@ -0,0 +1,25 @@
+package core.order;
+
+import core.discount.FixDiscountPolicy;
+import core.member.Grade;
+import core.member.Member;
+import core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
